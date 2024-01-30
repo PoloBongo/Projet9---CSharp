fe198c871ecb803729766e6939ad501388e7d054
quentin lf <quentinleforestier0@gmail.com>
2024-01-30 15:49:02 +0100

    tkt

--- Project/Initialisation.cs
+++ Project/Initialisation.cs
@@ -4,7 +4,7 @@
     {
         entity.Heal = 100;
         entity.Attack = 75;
-        entity.Aki = 50;
+        entity.Stamina = 50;
         entity.Precision = 90;
         entity.Speed = 60;
         entity.Level = 5;
