98ef000f50b5887d366ce31181d00357a8971343
0nnen <onnenennon@gmail.com>
2024-01-30 13:06:23 +0100

    Add Map & Basics Player Movements
    Version pas du tout terminée.
    Soucis majeur sur le déplacement des joueur

---
+++ Project/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerGame
+{
+    public class Player
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
