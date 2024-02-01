fb9f2805e44ad76ec72c51dbd9202a2b160276c9
0nnen <onnenennon@gmail.com>
2024-02-01 01:35:00 +0100

    Update Map-Village

---
+++ Project/Entities/Enemies/EnemyMap.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MapEntities;
+
+public class EnemyMap
+{
+    public int LocalX { get; set; }
+    public int LocalY { get; set; }
+    public int WorldX { get; set; }
+    public int WorldY { get; set; }
+
+    public EnemyMap(int worldX, int worldY, int localX, int localY)
+    {
+        WorldX = worldX;
+        WorldY = worldY;
+        LocalX = localX;
+        LocalY = localY;
+    }
+}
