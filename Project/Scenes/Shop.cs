8401a4aec7f38bc93c52defe2ec4ef545b5c04e1
unknown <lquintana@gamingcampus.local>
2024-02-02 09:33:58 +0100

    Boshop

--- Project/Scenes/Shop.cs
+++ Project/Scenes/Shop.cs
@@ -1,52 +1,138 @@
 ﻿using static System.Console;
 
+
 namespace ShopDemo
 {
     class Shop
     {
-        public static void Run()
-        {
-            Dictionary<string, double> produits = new Dictionary<string, double>()
+        private static Dictionary<string, double> produits = new Dictionary<string, double>()
         {
             { "Pommes", 1.50 },
             { "Bananes", 0.75 },
             { "Oranges", 2.00 },
             { "Fraises", 3.50 },
-            { "Pêches", 2.25 }
+            { "Pêches", 2.25 },
         };
 
-            WriteLine("Bienvenue dans notre magasin !");
-            WriteLine("Voici nos produits disponibles :");
+        private static int SelectedIndex = 0;
+        private static double money = 100.00;
 
-            foreach (var produit in produits)
+        public static void Run()
+        {
+            while (true)
             {
-                WriteLine($"{produit.Key}: {produit.Value:C}");
-            }
+                Clear();
+                DisplayProducts();
 
-            Write("Entrez le nom de l'article que vous souhaitez acheter : ");
-            string articleChoisi = ReadLine();
+                int produitChoisi = ChooseProduct();
 
-            if (produits.ContainsKey(articleChoisi))
-            {
+                string articleChoisi = produits.Keys.ElementAt(produitChoisi);
                 Write($"Entrez la quantité que vous souhaitez acheter de {articleChoisi} : ");
-                int quantite = int.Parse(ReadLine());
-
-                if (quantite > 0)
+                int quantite;
+                if (int.TryParse(ReadLine(), out quantite) && quantite > 0)
                 {
                     double prixTotal = produits[articleChoisi] * quantite;
-                    WriteLine($"Le prix total pour {quantite} {articleChoisi} est : {prixTotal:C}");
+                    if (money >= prixTotal)
+                    {
+                        money -= prixTotal;
+                        Clear();
+                        DisplayProducts(); // Afficher les informations mises à jour
+                        WriteLine($"Le prix total pour {quantite} {articleChoisi} est : {prixTotal}");
+                        WriteLine($"Il vous reste {money}.");
+                    }
+                    else
+                    {
+                        WriteLine("Fonds insuffisants pour cet achat.");
+                    }
                 }
                 else
                 {
                     WriteLine("La quantité doit être supérieure à zéro.");
                 }
-            }
-            else
-            {
-                WriteLine("Article non trouvé.");
-            }
 
-            WriteLine("Merci d'avoir magasiné avec nous !");
+                WriteLine("Appuyez sur une touche pour continuer...");
+                ReadKey();
+            }
+        }
+
+
+       
+    
+    private static int ChooseProduct()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                DisplayProducts();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex < 0)
+                    {
+                        SelectedIndex = produits.Count - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == produits.Count)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+
+
+        private static void DisplayProducts()
+        {
+
+
+            WriteLine(@"
+███████ ██   ██  ██████  ██████  
+██      ██   ██ ██    ██ ██   ██ 
+███████ ███████ ██    ██ ██████  
+     ██ ██   ██ ██    ██ ██      
+███████ ██   ██  ██████  ██      
+                                 
+                                 
+                                 
+                                 
+                                 ");
+
+
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine($"Argent disponible : {money}");
+            ResetColor();
+            WriteLine(" ");
+
+            WriteLine("Voici nos produits disponibles :");
+            int index = 0;
+            foreach (var produit in produits)
+            {
+                if (index == SelectedIndex)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                WriteLine($" <<{produit.Key}: {produit.Value}>>");
+                index++;
+            }
+            ResetColor();
         }
     }
 }
